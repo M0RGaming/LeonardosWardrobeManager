d30b010b7005e890ba6b098d1ad7b32f9b010d05
JoePShoulak <joepshoulak2@gmail.com>
2021-08-24 16:56:11 -0500

    Tutorial Starting Point

--- LeonardosWardrobeManager.lua
+++ LeonardosWardrobeManager.lua
@@ -5,52 +5,34 @@ LeonardosWardrobeManager = {}
 -- Better to define it in a single place rather than retyping the same string.
 LeonardosWardrobeManager.name = "LeonardosWardrobeManager"
 
--- Next we create a function that will initialize our addon
-function LeonardosWardrobeManager:Initialize()
-    -- ...but we don't have anything to initialize yet. We'll come back to this.
-end
-
--- Then we create an event handler function which will be called when the "addon loaded" event
--- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
-function LeonardosWardrobeManager:Initialize()
-    self.inCombat = IsUnitInCombat("player")
-
-    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
-
-    self.savedVariables = ZO_SavedVars:NewCharacterIdSettings("LeonardosWardrobeManagerSavedVariables", 1, nil, {})
-end
-
-function LeonardosWardrobeManager.OnIndicatorMoveStop()
-    LeonardosWardrobeManager.savedVariables.left = LeonardosWardrobeManagerIndicator:GetLeft()
-    LeonardosWardrobeManager.savedVariables.top = LeonardosWardrobeManagerIndicator:GetTop()
-end
-
-function LeonardosWardrobeManager:RestorePosition()
-    local left = self.savedVariables.left
-    local top = self.savedVariables.top
-
-    LeonardosWardrobeManagerIndicator:ClearAnchors()
-    LeonardosWardrobeManagerIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
-end
-
-function LeonardosWardrobeManager:Initialize()
-    self.inCombat = IsUnitInCombat("player")
-
-    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
-
-    self.savedVariables = ZO_SavedVars:NewCharacterIdSettings("LeonardosWardrobeManagerSavedVariables", 1, nil, {})
-
-    self:RestorePosition()
-end
-
 function LeonardosWardrobeManager.OnPlayerCombatState(event, inCombat)
     -- The ~= operator is "not equal to" in Lua.
     if inCombat ~= LeonardosWardrobeManager.inCombat then
         -- The player's state has changed. Update the stored state...
         LeonardosWardrobeManager.inCombat = inCombat
 
-        -- ...and then update the control.
-        LeonardosWardrobeManagerIndicator:SetHidden(not inCombat)
+        -- ...and then announce the change.
+        if inCombat then
+            d("Entering combat.")
+        else
+            d("Exiting combat.")
+        end
+
+    end
+end
+
+function LeonardosWardrobeManager:Initialize()
+    self.inCombat = IsUnitInCombat("player")
+
+    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
+end
+
+-- Then we create an event handler function which will be called when the "addon loaded" event
+-- occurs. We'll use this to initialize our addon after all of its resources are fully loaded.
+function LeonardosWardrobeManager.OnAddOnLoaded(event, addonName)
+    -- The event fires each time *any* addon loads - but we only care about when our own addon loads.
+    if addonName == LeonardosWardrobeManager.name then
+        LeonardosWardrobeManager:Initialize()
     end
 end
 
